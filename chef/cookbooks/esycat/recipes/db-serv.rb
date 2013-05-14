8706922c3e6d9729ac2053022659b57387685c49
Eugene Janusov <esycat@gmail.com>
2013-05-15 01:52:48 +1000

    db-serv recipe now added RethinkDB PPA.

--- chef/cookbooks/esycat/recipes/db-serv.rb
+++ chef/cookbooks/esycat/recipes/db-serv.rb
@@ -1,2 +1,10 @@
 package "mysql-server"
 
+apt_repository "rethinkdb" do
+    uri "http://ppa.launchpad.net/rethinkdb/ppa/ubuntu"
+    distribution node['lsb']['codename']
+    components ["main"]
+    keyserver "keyserver.ubuntu.com"
+    key "11D62AD6"
+end
+
