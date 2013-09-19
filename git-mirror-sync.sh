646aae3c8bd0d51b4d92705eacc2851a65fb884a
Eugene Janusov <esycat@gmail.com>
2013-09-19 12:29:56 +1000

    Improved the sync script.

--- git-mirror-sync.sh
+++ git-mirror-sync.sh
@@ -2,17 +2,27 @@
 
 source $HOME/.myenv
 
-repos_dir=$(dirname $(grealpath $0))
+realpath="realpath" # install `grealpath` for OS X
+repos_dir=$(dirname $($realpath $0))
+postuphook="hooks/post-update"
+
 LOG_FILE="${repos_dir}/up.log"
 LOG_AGENT="BackupAgent"
 
-cd "$repos_dir"
+GIT_USER="git"
+GIT_OPTS="-p"
+GIT_REMOTE="bb"
+
+if [[ -n "$GIT_USER"  && $(id -nu) != "$GIT_USER" ]] ; then
+    echo "This script must be executed as ${GIT_USER} user."
+    echo "Try: sudo -u ${GIT_USER} $0"
+    exit 1
+fi
 
 date >> $LOG_FILE
 logger -t $LOG_AGENT "Started backing up Git repositories"
 
-SRC_REMOTE="bb"
-GIT_OPTS="-p"
+cd "$repos_dir"
 
 if [ -t 1 ] ; then
     echolog="echo"
@@ -31,18 +41,18 @@ fi
 for repo in $repos ; do
     msg="Processing \`${repo%.git}\`..."
     logger -t $LOG_AGENT -p user.info "${LOG_PREFIX}: $msg"
-	$echolog $msg
+    $echolog $msg
 
-	if [ -d "$repo" ] ; then
-		cd "$repo"
-		git remote update $SRC_REMOTE $GIT_OPTS
-        ../post-update
-		cd "$repos_dir"
-	else
+    if [ -d "$repo" ] ; then
+        cd "$repo"
+        git remote update $GIT_REMOTE $GIT_OPTS
+        [[ -x "$postuphook" ]] && $postuphook
+        cd "$repos_dir"
+    else
         err_msg="$repo is not a valid repository"
         logger -t $LOG_AGENT -p user.error "${LOG_PREFIX}: ${err_msg}"
         $echolog $err_msg
-	fi
+    fi
 done
 
 logger -t $LOG_AGENT "Finished backing up Git repositories"
