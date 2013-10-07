c8b77982fcecb7cdb185950ca2d0aa98274c8c44
Eugene Janusov <esycat@gmail.com>
2013-10-07 20:14:04 +1100

    Added RethinkDB to db-serv role.

--- chef/roles/db-serv.rb
+++ chef/roles/db-serv.rb
@@ -2,7 +2,8 @@ name "db-serv"
 description "Database Server"
 run_list [
     "role[base-serv]",
-    "recipe[mysql]",
+    "recipe[mysql::server]",
+    "recipe[rethinkdb]",
     "recipe[esycat::db-serv]"
 ]
 
