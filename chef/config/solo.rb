9988d51fc83446a9c960261675ff17757cd60046
Eugene Janusov <esycat@gmail.com>
2013-10-21 21:22:44 +1100

    Updated Chef Solo config and start script.

--- chef/config/solo.rb
+++ chef/config/solo.rb
@@ -1,3 +1,13 @@
-cookbook_path File.expand_path(File.join(File.dirname(__FILE__), '..', "cookbooks"))
-role_path     File.expand_path(File.join(File.dirname(__FILE__), '..', "roles"))
+script_dir = File.dirname(File.realpath(__FILE__))
+config_dir = File.realpath(File.join(script_dir, '..'))
+
+cookbook_path [
+    '/var/chef/cookbooks',
+    File.realpath(File.join(config_dir, 'cookbooks'))
+]
+
+#role_path [
+#    '/var/chef/roles',
+#    File.realpath(File.join(config_dir, 'roles'))
+#]
 
--- chef/solo.sh
+++ chef/solo.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-script_dir="$(dirname $(readlink -f $0))"
-config_dir="${script_dir}/config"
+. /etc/default/chef-client
 
-chef-solo -c ${config_dir}/solo.rb -j ${config_dir}/node.json
+script_dir="$(dirname $(readlink -f $0))"
+#config_dir="${script_dir}/config"
+config_dir="/etc/chef"
+
+chef-solo -c "${config_dir}/solo.rb" -j "${config_dir}/node.json"
+
