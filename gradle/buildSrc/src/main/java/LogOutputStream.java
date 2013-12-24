fdaf783a0f4e54d34d6474334958d72c19a274a7
Eugene Janusov <esycat@gmail.com>
2013-12-24 19:48:42 +1100

    Gradle: More changes to LogOutputStream's docblock.

--- gradle/buildSrc/src/main/java/LogOutputStream.java
+++ gradle/buildSrc/src/main/java/LogOutputStream.java
@@ -4,13 +4,15 @@ import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.LogLevel;
 
 /**
+ * A simplistic OutputStream based on ByteArrayOutputStream that helps
+ * to redirect output of Gradle's Exec task directly to Gradle's Logger
+ * with the given log level.
+ *
  * @author Eugene Janusov <esycat@gmail.com>
+ * @link https://github.com/esycat/devops
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  *
- * This is a simplistic OutputStream that helps to redirect Gradle's Exec task
- * output directly to Gradle's Logger with the given log level.
- *
- * Example:
+ * @example:
  * task date (type: Exec) {
  *     executable 'date'
  *     standardOutput = new LogOutputStream(logger, LogLevel.INFO)
