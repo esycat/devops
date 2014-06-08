e1b550db6d0703eab89db8075318cd13e5012f73
Eugene Janusov <esycat@gmail.com>
2014-06-08 11:27:45 +1000

    Chef: More GSettings

--- chef/cookbooks/esycat/recipes/settings.rb
+++ chef/cookbooks/esycat/recipes/settings.rb
@@ -1,3 +1,56 @@
+### Nautilus
+
+gsettings 'org.gnome.nautilus.preferences' do
+    option 'default-folder-viewer'
+    user 'esycat'
+    value 'list-view'
+end
+
+gsettings 'org.gnome.nautilus.preferences' do
+    option 'sort-directories-first'
+    user 'esycat'
+    value 'true'
+end
+
+
+### Keyboard
+
+gsettings 'org.gnome.desktop.input-sources' do
+    option 'sources'
+    user 'esycat'
+    value '[("xkb", "us"), ("xkb", "ru")]'
+end
+
+gsettings 'org.gnome.desktop.input-sources' do
+    option 'xkb-options'
+    user 'esycat'
+    value '[
+        "apple:alupckeys",
+        "compose:ralt",
+        "grp_led:caps",
+        "numpad:pc",
+        "terminate:ctrl_alt_bksp:q"
+    ]'
+end
+
+gsettings 'org.gnome.libgnomekbd.desktop' do
+    option 'group-per-window'
+    user 'esycat'
+    value 'true'
+end
+
+gsettings 'org.gnome.libgnomekbd.indicator' do
+    option 'show-flags'
+    user 'esycat'
+    value 'true'
+end
+
+gsettings 'org.gnome.settings-daemon.peripherals.keyboard' do
+    option 'numlock-state'
+    user 'esycat'
+    value 'off'
+end
+
 gsettings 'org.gnome.desktop.wm.keybindings' do
     option 'switch-input-source'
     user 'esycat'
@@ -7,35 +60,83 @@ end
 gsettings 'org.gnome.settings-daemon.plugins.media-keys' do
     option 'screensaver'
     user 'esycat'
-    value ['Scroll_Lock']
+    value ['<Primary>Escape', 'Scroll_Lock']
 end
 
-gsettings 'org.gnome.nautilus.preferences' do
-    option 'default-folder-viewer'
+
+### Desktop
+
+gsettings 'org.gnome.desktop.wm.preferences' do
+    option 'workspace-names'
     user 'esycat'
-    value 'list-view'
+    value '[
+        "Main",
+        "Communication",
+        "Devel"
+    ]'
 end
 
-gsettings 'org.gnome.desktop.input-sources' do
-    option 'sources'
+gsettings 'org.gnome.desktop.screensaver' do
+    option 'lock-delay'
     user 'esycat'
-    value '[("xkb", "us"), ("xkb", "ru")]'
+    value '5'
 end
 
-gsettings 'org.gnome.settings-daemon.peripherals.keyboard' do
-    option 'numlock-state'
+gsettings 'org.gnome.desktop.interface' do
+    option 'clock-format'
     user 'esycat'
-    value 'off'
+    value '24h'
 end
 
-gsettings 'org.gnome.libgnomekbd.desktop' do
-    option 'group-per-window'
+gsettings 'org.gnome.desktop.interface' do
+    option 'clock-show-date'
     user 'esycat'
     value 'true'
 end
 
-gsettings 'org.gnome.desktop.input-sources' do
-    option 'xkb-options'
+gsettings 'org.gnome.desktop.interface' do
+    option 'show-input-method-menu'
     user 'esycat'
-    value '["compose:ralt"]'
+    value 'true'
 end
+
+gsettings 'com.canonical.Unity.Launcher' do
+    option 'favorites'
+    user 'esycat'
+    value '[
+        "application://nautilus.desktop",
+        "application://gnome-terminal.desktop",
+        "application://google-chrome-beta.desktop",
+        "application://firefox.desktop",
+        "application://thunderbird.desktop",
+        "application://skype.desktop",
+        "application://sublime-text.desktop",
+        "application://idea.desktop",
+        "application://gitg.desktop",
+        "application://virtualbox.desktop",
+        "unity://running-apps",
+        "unity://expo-icon",
+        "unity://devices"
+    ]'
+end
+
+gsettings 'com.canonical.Unity.Lenses' do
+    option 'disabled-scopes'
+    user 'esycat'
+    value '[
+        "more_suggestions-amazon.scope",
+        "more_suggestions-u1ms.scope",
+        "more_suggestions-populartracks.scope",
+        "music-musicstore.scope",
+        "more_suggestions-ebay.scope",
+        "more_suggestions-ubuntushop.scope",
+        "more_suggestions-skimlinks.scope"
+    ]'
+end
+
+
+
+
+
+
+
