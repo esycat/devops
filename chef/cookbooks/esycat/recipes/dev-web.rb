2d41be9607e23c513521db4fb092d307b9b08309
Eugene Janusov <esycat@gmail.com>
2014-06-24 16:52:38 +1000

    Chef: Node.js, Bower, and other important things for web development

--- chef/cookbooks/esycat/recipes/dev-web.rb
+++ chef/cookbooks/esycat/recipes/dev-web.rb
@@ -1,5 +1,17 @@
-package "gem"
+package 'gem'
 
-gem_package "sass"
-gem_package "scss-lint"
+gem_package 'sass'
+gem_package 'scss-lint'
+gem_package 'compass'
 
+# package 'nodejs'
+# package 'npm'
+
+include_recipe 'nodejs'
+include_recipe 'nodejs::npm'
+
+nodejs_npm 'request'
+nodejs_npm 'async'
+nodejs_npm 'socket.io'
+nodejs_npm 'jquery'
+nodejs_npm 'bower'
