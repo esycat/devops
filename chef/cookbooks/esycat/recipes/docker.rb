39de1433b10232c52f8b5bca220143cbe468cb9a
Eugene Janusov <esycat@gmail.com>
2014-07-07 19:53:06 +1000

    Chef: Somewhat failed attempt to add Docker cookbook (too many deps)

---
+++ chef/cookbooks/esycat/recipes/docker.rb
@@ -0,0 +1,4 @@
+# node.set['docker']['package']['repo_url'] = 'https://get.docker.io/ubuntu'
+# include_recipe 'docker::package'
+# include_recipe 'docker::systemd'
+# include_recipe 'docker::upstart'
