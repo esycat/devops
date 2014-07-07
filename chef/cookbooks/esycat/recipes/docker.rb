4dd244b7363908d6cf57e9222a832e737f0643fc
Eugene Janusov <esycat@gmail.com>
2014-07-07 20:01:59 +1000

    Chef: Let's try to install Docker manually

--- chef/cookbooks/esycat/recipes/docker.rb
+++ chef/cookbooks/esycat/recipes/docker.rb
@@ -1,3 +1,18 @@
+apt_repository 'docker' do
+    uri          'https://get.docker.io/ubuntu'
+    distribution 'docker'
+    components   ['main']
+    keyserver    'keyserver.ubuntu.com'
+    key          '36A1D7869245C8950F966E92D8576A8BA88D21E9'
+end
+
+package 'docker.io'
+package 'lxc-docker'
+
+link '/usr/local/bin/docker' do
+    to '/usr/bin/docker.io'
+end
+
 # node.set['docker']['package']['repo_url'] = 'https://get.docker.io/ubuntu'
 # include_recipe 'docker::package'
 # include_recipe 'docker::systemd'
