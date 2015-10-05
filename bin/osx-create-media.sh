29d000a13025132246cae064af51d253dbcc02d1
Eugene Janusov <esycat@gmail.com>
2015-10-05 21:46:45 +1100

    Update the shell script to create El Capitan bootable USB

--- bin/osx-create-media.sh
+++ bin/osx-create-media.sh
@@ -6,10 +6,13 @@
 # http://www.macworld.com/article/2056561/how-to-make-a-bootable-mavericks-install-drive.html
 #
 
-OSX_NAME="Yosemite"
+DEVICE="/dev/disk3"
+OSX_NAME="El Capitan"
 APP_NAME="Install OS X ${OSX_NAME}"
 APP_PATH="/Applications"
 APP_FILE="${APP_PATH}/${APP_NAME}.app"
+
+# sudo diskutil partitionDisk $DEVICE 1 GPT JHFS+ "${OSX_NAME}" R
 
 sudo "${APP_FILE}/Contents/Resources/createinstallmedia" \
     --volume "/Volumes/${OSX_NAME}" \
